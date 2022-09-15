41d2ff7953b859d806d1f6d4ced94bfef2911003
ChristelDeveza <chris.dvz66@gmail.com>
2022-09-15 15:49:48 +0200

    homepage structure

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../CSS/Homepage.css';
+import CatHero from '../img/cat-hero.jpg';
 
 function Homepage() {
   return (
-    <div>
-      <h1>Accueil</h1>
+    <div className='Homepage'>
+      <img className='cat-hero' src={CatHero} alt='cat-hero' />
+      <h1 className='Header'>
+        Votre comparateur de croquettes simple et rapide{' '}
+      </h1>
+      <p className='text-header'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
+        accusantium cupiditate nam sed repellendus magnam dicta dolorem corrupti
+        eos dignissimos, perspiciatis quibusdam omnis similique consequuntur
+        quaerat totam eveniet voluptatibus vitae?
+      </p>
     </div>
   );
 }
