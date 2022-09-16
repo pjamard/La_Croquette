20c6c0d0bc51f263a5593f9f45fcca248c38bdfc
ChristelDeveza <chris.dvz66@gmail.com>
2022-09-16 13:26:45 +0200

    footer and CSS

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import '../CSS/Homepage.css';
-import CatHero from '../img/cat-hero.jpg';
+import CatHero from '../images/cat-hero.jpg';
+import Chat from '../images/chat.png';
+import Footer from '../components/Footer';
 
 function Homepage() {
   return (
     <div className='Homepage'>
-      <img className='cat-hero' src={CatHero} alt='cat-hero' />
-      <h1 className='Header'>
-        Votre comparateur de croquettes simple et rapide{' '}
-      </h1>
-      <p className='text-header'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
-        accusantium cupiditate nam sed repellendus magnam dicta dolorem corrupti
-        eos dignissimos, perspiciatis quibusdam omnis similique consequuntur
-        quaerat totam eveniet voluptatibus vitae?
-      </p>
+      <div className='contenant-cat-hero-image'>
+        <img className='cat-hero' src={CatHero} alt='cat-hero' />
+        <div className='texte-on-cat-image'>LA CROQUETTE</div>
+      </div>
+      <div>
+        <h1 className='Header'>
+          Votre comparateur de croquettes simple et rapide{' '}
+        </h1>
+        <p className='text-header'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
+          accusantium cupiditate nam sed repellendus magnam dicta dolorem
+          corrupti eos dignissimos, perspiciatis quibusdam omnis similique
+          consequuntur quaerat totam eveniet voluptatibus vitae?
+          <br /> <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
+          accusantium cupiditate nam sed repellendus magnam dicta dolorem
+          corrupti eos dignissimos, perspiciatis quibusdam omnis similique
+          consequuntur quaerat totam eveniet voluptatibus vitae?
+        </p>
+      </div>
+      <div>
+        <img className='cat-icon' src={Chat} alt='cat' />
+      </div>
+      <Footer />
     </div>
   );
 }
